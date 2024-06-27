6ca33b1839aa3faf8fb05a147455542cab122c55
netcigos <netcigos@hotmail.com>
2024-06-27 20:12:57 -0300

    Juego actualisado

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 
-let numeroSecreto= generarNumeroSecreto();
+let numeroSecreto=0;
+let intentos=0;
+condicionesInicio();
+
 console.log(numeroSecreto);
 
 function asignarTextoElemento(elemento, texto)
@@ -22,10 +25,57 @@ function verificarIntento()
 {
    let valorUsuario= parseInt(document.getElementById('valorUsuario').value);
    console.log(valorUsuario);
-   console.log(numeroSecreto === valorUsuario);
+  
+   if(numeroSecreto===valorUsuario)
+      {
+        asignarTextoElemento('p',`Hacertaste el numero en ${intentos} ${(intentos==1) ? "ves" : "veces"}` );
+          
+        limpiarCaja();
+        document.getElementById('reiniciar').removeAttribute('disabled');
+        document.querySelector('intentar').setAttribute('disabled',true);
+
+
+      }else{
+
+      
+      if(numeroSecreto > valorUsuario)
+         {
+           asignarTextoElemento('p','El numero secreto es mayor')
+
+         }
+         else{
+
+          asignarTextoElemento('p','El numero secreto es menor');
+
+         }
+         intentos++;
+         limpiarCaja();
+
+      }
+  
 
   return;
 }
+function condicionesInicio()
+{
+   numeroSecreto=generarNumeroSecreto();
+   intentos=1;
+   asignarTextoElemento('h1',"Juego secreto");
+   asignarTextoElemento('p',"Elija un numero de 1 - 10");
+   
 
-asignarTextoElemento('h1',"Juego secreto");
-asignarTextoElemento('p',"Elija un numero de 1 - 100");
+}
+function limpiarCaja()
+{
+   document.querySelector('#valorUsuario').value='';
+
+}
+
+function reiniciarJuego()
+{
+  limpiarCaja();
+  condicionesInicio();
+  document.querySelector('reiniciar').setAttribute('disabled',true);
+}
+
+
